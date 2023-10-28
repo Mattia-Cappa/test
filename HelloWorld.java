d46ff7187e60c12e8cc15e49a46a3d9fc4d2351d
Mattia-Cappa <mcappa@cerebotani.it>
2023-10-28 08:14:07 +0200

    file che somma

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,16 @@
 import java.io.*;
 
 class HelloWorld{
+	
+	
 	public static void main(String args[]){
 		System.out.println("hello world!");
+		int  n1 = 2;
+		int  n2 = 4;
+		System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
+		
+		
 	}
+	
+	
 }
